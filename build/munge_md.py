1d8d8e9fc3b615eea5d0e78ab89825142743998a
Kyle Steinfeld <ksteinfe@gmail.com>
2018-03-26 13:36:10 -0700

    grafs

--- build/munge_md.py
+++ build/munge_md.py
@@ -1,11 +1,11 @@
 print("munge_md.py has loaded")
 
 global md_to_html
-def md_to_html(mdfile):
-    f = "head!"  
+def md_to_html(mdfile, fragments):
+    f = fragments['head'] 
     f += mistune.markdown(mdfile)
     # for line in mdfile: f += line
-    f += "foot"
+    f += fragments['foot'] 
     
     
     return io.StringIO(unicode(f))
