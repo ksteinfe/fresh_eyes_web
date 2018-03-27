039b0159db0d6c77a9691307faf4955481133ec6
Kyle Steinfeld <ksteinfe@gmail.com>
2018-03-26 23:19:54 -0700

    ^

--- build/munge_md.py
+++ build/munge_md.py
@@ -4,52 +4,24 @@ print("munge_md.py has loaded..")
 # consider making changes to the lambda function to flush things out
 global md_to_html
 def md_to_html(mdfile, fragments):
-    
-    '''
-    class WikiLinkRenderer(Renderer):
-        def wiki_link(self, alt, link):
-            return '<a href="%s">%s</a>' % (link, alt)
-
-    class WikiLinkInlineLexer(InlineLexer):
-        def enable_wiki_link(self):
-            # add wiki_link rules
-            self.rules.wiki_link = re.compile(
-                r'\[\['                   # [[
-                r'([\s\S]+?\|[\s\S]+?)'   # Page 2|Page 2
-                r'\]\](?!\])'             # ]]
-            )
-
-            # Add wiki_link parser to default rules
-            # you can insert it some place you like
-            # but place matters, maybe 3 is not good
-            self.default_rules.insert(3, 'wiki_link')
-
-        def output_wiki_link(self, m):
-            text = m.group(1)
-            alt, link = text.split('|')
-            # you can create an custom render
-            # you can also return the html if you like
-            return self.renderer.wiki_link(alt, link)
-    '''
-    
+        
     class FreshEyesRenderer(Renderer):
         def section_marker(self, stuff):
             print("afds"+str(stuff))
-            return '</div><div class="section">{}'.format(stuff)
+            return '</div><div class="c-item-v2__section">{}'.format(stuff)
     
             
     class FreshEyesInlineLexer(InlineLexer):
         def enable_fresh_eyes(self):
             # add section_marker rules
             self.rules.section_marker = re.compile( r'\[\[section\|([\s\S]+?)\]\]' )
-            # Add section_marker parser to default rules
-            # you can insert it some place you like
-            # but place matters, maybe 3 is not good
             self.default_rules.insert(3, 'section_marker')
 
         def output_section_marker(self, m):
             stuff = m.group(1)
             return self.renderer.section_marker(stuff)
+    
+    
     
     renderer = FreshEyesRenderer()
     inline = FreshEyesInlineLexer(renderer)
