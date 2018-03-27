9d87701ac2e33fe38119d51237e71aecc7109a60
Kyle Steinfeld <ksteinfe@gmail.com>
2018-03-26 21:16:22 -0700

    delete

--- build/munge_md.py
+++ build/munge_md.py
@@ -3,31 +3,6 @@ print("munge_md.py has loaded")
 global md_to_html
 def md_to_html(mdfile, fragments):
     
-    '''
-    class FreshEyesRenderer(mistune.Renderer):
-        def image(self, src, title, alt_text):
-            return '<img style="width: auto;" src="{}" alt="{}" title="{}">'.format(src, title, alt_text)
-    
-    
-    class EmojiRenderer(object):
-        def emoji(self, text):
-            return "<emoji>%s</emoji>" % text
-
-    class EmojiInlineLexer(mistune.InlineLexer):
-        def __init__(self, **kwargs):
-            super(EmojiInlineLexer, self).__init__(**kwargs)
-            self.default_rules.insert(0, "emoji")
-            self.rules.emoji = re.compile(r'^:([a-zA-Z0-9\+\-_]+):', re.I)
-
-        def output_emoji(self, m):
-            text = self.output(m.group(1))
-            return self.renderer.emoji(text)
-
-
-    class MarkdownRenderer(mistune.Renderer, EmojiRenderer):
-        def __init__(self, **kwargs):
-            super(MarkdownRenderer, self).__init__(**kwargs)
-    '''
     from mistune import Markdown, Renderer, InlineGrammar, InlineLexer
     
     class WikiLinkRenderer(Renderer):
@@ -52,13 +27,6 @@ def md_to_html(mdfile, fragments):
             # you can create an custom render
             # you can also return the html if you like
             return self.renderer.wiki_link(alt, link)
-    
-    
-    '''
-    renderer = MarkdownRenderer()
-    inline = EmojiInlineLexer(renderer=renderer)
-    markdown = mistune.Markdown(renderer=renderer, inline=inline)
-    '''
     
     renderer = WikiLinkRenderer()
     inline = WikiLinkInlineLexer(renderer=renderer)
